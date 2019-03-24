75fe77c38392d9fa190aa26a0a60893b507d9d84
Rachel Sleet <rachelsleet@gmail.com>
2019-03-24 22:45:22 +0100

    Updates

---
+++ precache-manifest.aa8200569281dc1a07a790ac47bba2a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "4a985d65249a88ccb429",
+    "url": "/calculator/static/js/main.cd1857ac.chunk.js"
+  },
+  {
+    "revision": "ad67d665b114896bcf1d",
+    "url": "/calculator/static/js/2.58b3c62a.chunk.js"
+  },
+  {
+    "revision": "4a985d65249a88ccb429",
+    "url": "/calculator/static/css/main.3d666d68.chunk.css"
+  },
+  {
+    "revision": "d6a94524d4a261844ae8e4994f6f783f",
+    "url": "/calculator/index.html"
+  }
+];
